779dd3e9ecd4fca7bffefca7e1fcb4ebf35b0330
eri <a22310383@ceti.mx>
2025-03-25 15:15:06 -0600

    initial commit :p

---
+++ app/src/main/java/com/example/introduccionfigma/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.introduccionfigma
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.introduccionfigma.atributos.Atributos
+import com.example.introduccionfigma.ui.theme.IntroduccionFigmaTheme
+import com.example.introduccionfigma.welcome.Welcome
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IntroduccionFigmaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Welcome()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IntroduccionFigmaTheme {
+        Atributos()
+    }
+}
